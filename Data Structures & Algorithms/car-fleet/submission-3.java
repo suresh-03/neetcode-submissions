1ae3c9c2fa09e3acb73bae7ae83ef2e65dd5ba30
suresh kumar <sureshkumar732003@gmail.com>
2026-06-21 19:09:17 +0530

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+        Pair[] pairs = new Pair[n];
+
+        for (int i = 0; i < n; i++) {
+            pairs[i] = new Pair(position[i], speed[i]);
+            
+        }
+
+        Arrays.sort(pairs, (a, b) -> Integer.compare(a.position, b.position));
+
+        Stack<Pair> stk = new Stack<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+           
+            if (stk.isEmpty()) {
+                stk.push(pairs[i]);
+            } else {
+                int pos = pairs[i].position;
+                int s = pairs[i].speed;
+
+                double estimatedTimeToReach = ((double)(target - pos)) / (double)s;
+                double estimatedTimeToReachStack = ((double)(target - stk.peek().position)) / (double)stk.peek().speed;
+
+                if(estimatedTimeToReachStack < estimatedTimeToReach){
+                    stk.push(pairs[i]);
+                }
+            }
+  
+        }
+
+        return stk.size();
+    }
+}
+
+class Pair {
+    int position;
+    int speed;
+
+    public Pair(int position, int speed) {
+        this.position = position;
+        this.speed = speed;
+    }
+
+    public String toString(){
+        return "("+this.position+","+this.speed+")";
+    }
+}
