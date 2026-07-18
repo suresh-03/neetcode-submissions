f900005696ffa9018c96e499dacf4eb8a833b0f9
suresh kumar <sureshkumar732003@gmail.com>
2026-07-18 08:29:02 +0530

    Add: k-closest-points-to-origin - submission-0

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-0.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        int[][] kClosestPoints = new int[k][2];
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>(new DistanceComparator());
+
+        for(int i = 0; i < points.length; i++){
+            double distance = calculateDistance(points[i]);
+            pq.add(new Pair(i, distance));
+        }
+
+        for(int i = 1; i <= k; i++){
+            Pair p = pq.poll();
+
+            kClosestPoints[i-1] = points[p.index];
+        }
+
+        return kClosestPoints;
+
+
+    }
+
+    double calculateDistance(int[] point){
+        double n1 = 0 - point[0];
+        double n2 = 0 - point[1];
+
+        double sqrSum = (n1 * n1) + (n2 * n2);
+
+        return Math.sqrt(sqrSum);
+    }
+}
+
+public class DistanceComparator implements Comparator<Pair>{
+    public int compare(Pair p1, Pair p2){
+        return Double.compare(p1.distance, p2.distance);
+    }
+}
+
+class Pair{
+    int index;
+    double distance;
+
+    public Pair(int index, double distance){
+        this.index = index;
+        this.distance = distance;
+    }
+}
