61a4683263a75eec5be4ba8ab25d777b95418cfe
suresh kumar <sureshkumar732003@gmail.com>
2026-07-25 07:50:44 +0530

    Add: combinations-of-a-phone-number - submission-0

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-0.java
@@ -0,0 +1,38 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        if(digits.equals("")){
+            return new ArrayList<String>();
+        }
+        List<String> ans = new ArrayList<>();
+        char[][] characters = {
+            {},
+            {},
+            {'a','b','c'},
+            {'d','e','f'},
+            {'g','h','i'},
+            {'j','k','l'},
+            {'m','n','o'},
+            {'p','q','r','s'},
+            {'t','u','v'},
+            {'w','x','y','z'},
+        };
+
+        generate(digits,characters,ans,"",0);
+        return ans;
+    }
+
+    public void generate(String digits, char[][] characters, List<String> ans, String str, int index){
+        if(digits.length() == str.length() || index == digits.length()){
+            ans.add(str);
+            return;
+        }
+
+        char[] charArray = characters[digits.charAt(index)-'0'];
+
+        for(int i = 0; i < charArray.length; i++){
+            char ch = charArray[i];
+            generate(digits,characters,ans,str+ch,index+1);
+            // str = str.substring(0,str.length()-1);
+        }
+    }
+}
