0bb05efec2dc4f14c387791301c64d769474dfe0
suresh kumar <sureshkumar732003@gmail.com>
2026-07-26 11:54:32 +0530

    Add: design-word-search-data-structure - submission-3

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-3.java
@@ -0,0 +1,90 @@
+class WordDictionary {
+    Trie root;
+    public WordDictionary() {
+        this.root = new Trie();
+    }
+
+    public void addWord(String word) {
+        Trie temp = root;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            if (!temp.contains(ch)) {
+                temp.setTrie(ch);
+            }
+            temp = temp.getTrie(ch);
+        }
+        temp.setFlag(true);
+    }
+
+    public boolean search(String word) {
+        Trie temp = root;
+        return search(temp, word, 0);
+    }
+
+    public boolean search(Trie temp, String word, int index) {
+        for (int i = index; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            if (word.equals(".at")) {
+                System.out.println(ch);
+            }
+            if (ch == '.') {
+                for (int j = 0; j < 26; j++) {
+                    if (temp.contains((char) (j + 97))) {
+                        if (word.equals(".at")) {
+                            System.out.println("char: " + (char) (j + 97));
+                        }
+                        Trie t = temp;
+                        temp = temp.getTrie((char) (j + 97));
+                        if (search(temp, word, index + 1)) {
+                            if (word.equals(".at")) {
+                                System.out.println("inside");
+                            }
+                            return true;
+                        }
+                        else{
+                            temp = t;
+                        }
+                    }
+                }
+                return false;
+            } else if (!temp.contains(ch)) {
+                return false;
+            }
+            if (ch != '.') {
+                temp = temp.getTrie(ch);
+            }
+            index++;
+        }
+        return temp.getFlag();
+    }
+}
+
+class Trie {
+    private Trie[] trie;
+    private boolean flag;
+
+    public Trie() {
+        this.trie = new Trie[26];
+        this.flag = false;
+    }
+
+    public boolean contains(char ch) {
+        return this.trie[ch - 'a'] != null;
+    }
+
+    public void setTrie(char ch) {
+        this.trie[ch - 'a'] = new Trie();
+    }
+
+    public Trie getTrie(char ch) {
+        return this.trie[ch - 'a'];
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+
+    public boolean getFlag() {
+        return this.flag;
+    }
+}
