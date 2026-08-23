beefab8a01c0f49e18d4fb2b3e96346d2f97194c
suresh kumar <sureshkumar732003@gmail.com>
2026-08-23 10:20:03 +0530

    Add: word-break - submission-3

---
+++ Data Structures & Algorithms/word-break/submission-3.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int[] dp = new int[s.length()];
+        for (int i = 0; i < dp.length; i++) {
+            dp[i] = -1;
+        }
+        return solve(0, s, wordDict, dp);
+    }
+
+    public boolean solve(int index, String s, List<String> wordDict, int[] dp) {
+        if (index >= s.length()) {
+            return true;
+        }
+
+        if (dp[index] != -1) {
+            return dp[index] == 0 ? false : true;
+        }
+
+        for (int i = 0; i < wordDict.size(); i++) {
+            if (index + wordDict.get(i).length() <= s.length()) {
+                if (s.substring(index, index + wordDict.get(i).length()).equals(wordDict.get(i))) {
+                    if (solve(index + wordDict.get(i).length(), s, wordDict, dp)) {
+                        dp[index] = 1;
+                        return true;
+                    }
+                }
+            }
+        }
+        dp[index] = 0;
+        return false;
+    }
+}
